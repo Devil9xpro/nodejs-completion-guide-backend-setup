1bcaf82a1e53d4a51c494d0acf327bd5bffe3259
David Nguyen <chinhzz3107@gmail.com>
2021-10-18 21:51:22 +0700

    upload file and store in image folder

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
 const express = require('express')
 const moongose = require('mongoose')
 const path = require('path')
+const multer = require('multer')
+const { v4: uuidv4 } = require('uuid');
 
 const app = express()
+
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images')
+    },
+    filename: (req, file, cb) => {
+        cb(null, uuidv4() + '-' + file.originalname);
+    }
+})
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg'
+    ) {
+        cb(null, true)
+    } else {
+        cb(null, false)
+    }
+}
 
 const feedRoutes = require('./routes/feed')
 
 app.use(express.json())
-
-app.use('/images', express.static(path.join(__dirname, 'images')))
+app.use(
+    multer({
+        storage: fileStorage,
+        fileFilter: fileFilter
+    }).single('image')
+)
 
 // Handle CORS error
 app.use((req, res, next) => {
